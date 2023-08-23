009a6735a11c42dd164ba6a18cc4ab981cf9f9ba
acchu <acchu@gmail.com>
2023-08-23 09:28:50 +0530

    chnage1

---
+++ p6.py
@@ -0,0 +1,20 @@
+import pandas as pd
+data=pd.DataFrame([[1,2,3],[8,57,4]],columns=["maths","science","kannada"])
+print(data)
+
+
+
+import pandas as pd
+data1={"name":["racchu","acchu","nagu","swetha"],"age":[19,18,21,23]}
+print(pd.DataFrame(data1))
+
+import pandas as pd
+data2=pd.DataFrame({"name":["x","y","x","a"],"age":[1,8,2,23]})
+print((data2))
+
+
+
+
+
+
+
